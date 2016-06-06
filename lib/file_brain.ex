3b9036ea88e3fa0090b9ea6faed29f06fef6022e
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2016-06-06 21:02:23 +0900

    Add FileBrain

---
+++ lib/file_brain.ex
@@ -0,0 +1,66 @@
+use Croma
+
+defmodule Lambda.FileBrain do
+  alias Croma.Result, as: R
+  @random_bits_length 2
+  @brain_path Path.expand(Path.join([".", "brain"]))
+  @type error :: {:error, File.posix}
+  @type find_t :: map | [String.t]
+
+  defun find(db_name :: v[String.t], col_name :: v[String.t], id :: nil | String.t \\ nil) :: R.t(find_t) do
+    case id do
+      nil       ->
+        col_path = Path.join([@brain_path, db_name, col_name])
+        File.ls(col_path)
+      file_name ->
+        doc_path = Path.join([@brain_path, db_name, col_name, file_name])
+        File.read(doc_path) |> R.map(&Poison.decode!/1)
+    end
+  end
+
+  defun insert(db_name :: v[String.t], col_name :: v[String.t], id :: nil | String.t \\ nil, doc :: v[map]) :: R.t(String.t) do
+    file_name = id || generate_id
+    col_path = Path.join([@brain_path, db_name, col_name])
+    File.mkdir_p!(col_path) # Idempotent. Rarely fails
+    doc_path = Path.join(col_path, file_name)
+    case File.write(doc_path, Poison.encode!(doc)) do
+      :ok -> {:ok, file_name}
+      e   -> e
+    end
+  end
+
+  defun update(db_name :: v[String.t], col_name :: v[String.t], id :: v[String.t], doc :: v[map]) :: R.t(String.t) do
+    case find(db_name, col_name, id) do
+      {:ok, map} -> insert(db_name, col_name, id, Map.merge(map, doc))
+      e          -> e
+    end
+  end
+
+  defun remove(db_name :: v[String.t], col_name :: v[String.t], id :: v[String.t]) :: R.t(String.t) do
+    remove_path(Path.expand(Path.join([@brain_path, db_name, col_name, id])))
+  end
+
+  defun drop_db(db_name :: v[String.t]) :: R.t(String.t) do
+    remove_path(Path.expand(Path.join([@brain_path, db_name])))
+  end
+
+  defun drop_col(db_name :: v[String.t], col_name :: v[String.t]) :: R.t(String.t) do
+    remove_path(Path.expand(Path.join([@brain_path, db_name, col_name])))
+  end
+
+  # Time-based sortable random ID
+  defp generate_id do
+    random_bits   = Base.encode16(:crypto.strong_rand_bytes(@random_bits_length))
+    timestamp_str = SolomonLib.Time.now |> SolomonLib.Time.to_gregorian_milliseconds |> Integer.to_string(36)
+    timestamp_str <> random_bits
+  end
+
+  defp remove_path(path) do
+    case File.rm_rf(path) do
+      {:ok, _}               -> {:ok, String.replace_prefix(path, @brain_path <> "/", "")}
+      {:error, _posix, file} -> {:error, file}
+    end
+  end
+
+  defpt location, do: @brain_path
+end
---
+++ test/lib/file_brain_test.exs
@@ -0,0 +1,76 @@
+defmodule Lambda.FileBrainTest do
+  use Croma.TestCase, alias_as: FB
+
+  @db "test_db"
+  @col "test_col"
+  @d %{"data" => "value"}
+
+  setup do: on_exit(&remove_brain/0)
+
+  defp remove_brain, do: File.rm_rf(FB.location)
+
+  test "insert/4 should insert data as file" do
+    {:ok, file_name} = FB.insert(@db, @col, @d)
+    doc_path = Path.join([FB.location, @db, @col, file_name])
+    assert File.read!(doc_path) == "{\"data\":\"value\"}"
+  end
+
+  test "insert/4 should insert data as file with given id" do
+    id = "doc01"
+    {:ok, file_name} = FB.insert(@db, @col, id, @d)
+    assert file_name == id
+  end
+
+  test "find/3 should get data as map" do
+    {:ok, id} = FB.insert(@db, @col, @d)
+    {:ok, doc} = FB.find(@db, @col, id)
+    assert doc == @d
+  end
+
+  test "find/3 should get doc list within a col" do
+    {:ok, id1} = FB.insert(@db, @col, @d)
+    {:ok, id2} = FB.insert(@db, @col, @d)
+    {:ok, list} = FB.find(@db, @col)
+    assert list == [id1, id2] # IDs sorted by time
+  end
+
+  test "update/4 should update a doc" do
+    {:ok, id1} = FB.insert(@db, @col, @d)
+    new_doc = %{"data" => "updated", "add" => "field"}
+    {:ok, id2} = FB.update(@db, @col, id1, new_doc)
+    assert id1 == id2
+    {:ok, doc} = FB.find(@db, @col, id2)
+    assert doc == new_doc
+  end
+
+  test "remove/3 should remove a doc" do
+    {:ok, id1} = FB.insert(@db, @col, @d)
+    {:ok, id2} = FB.insert(@db, @col, @d)
+    {:ok, list1} = FB.find(@db, @col)
+    assert list1 == [id1, id2]
+    {:ok, path} = FB.remove(@db, @col, id1)
+    assert path == Path.join([@db, @col, id1])
+    {:ok, list2} = FB.find(@db, @col)
+    assert list2 == [id2]
+  end
+
+  test "drop_db/1 should delete db" do
+    {:ok, id} = FB.insert(@db, @col, @d)
+    {:ok, db} = FB.drop_db(@db)
+    assert db == @db
+    assert {:error, :enoent} == FB.find(@db, @col, id)
+    db_path = Path.join([FB.location, @db])
+    refute File.exists?(db_path)
+  end
+
+  test "drop_col/2 should delete col" do
+    {:ok, id} = FB.insert(@db, @col, @d)
+    {:ok, col} = FB.drop_col(@db, @col)
+    assert col == @db <> "/" <> @col
+    assert {:error, :enoent} == FB.find(@db, @col, id)
+    db_path = Path.join([FB.location, @db])
+    assert File.exists?(db_path)
+    col_path = Path.join(db_path, @col)
+    refute File.exists?(col_path)
+  end
+end
