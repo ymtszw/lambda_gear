189dfb0f427c20442190fc270fd548e1e018a20a
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2016-06-07 23:03:54 +0900

    Implement form

--- web/static/js/root.js
+++ web/static/js/root.js
@@ -1,8 +1,3 @@
-$(function() {
-  $('#contents').loadTemplate('static/js/templates/list.html', {});
-  loadFuncs();
-});
-
 function loadFuncs() {
   $.getJSON('functions', null, function(json) {
     $.each(json.items, function(id, func) {
@@ -19,3 +14,30 @@ function loadFuncs() {
 function showForm() {
   $('#contents').loadTemplate('static/js/templates/form.html', {});
 };
+
+function showList() {
+  $('#contents').loadTemplate('static/js/templates/list.html', {});
+  loadFuncs();
+};
+
+function postFunc() {
+  var title = $('#function_title').val()
+  var path = $('#function_path').val()
+  var code = $('#function_code').val()
+  if(path.length > 0 && code.length > 0) {
+    var data = {
+      path: path,
+      code: code
+    }
+    if(title.length > 0) {data.title = title}
+    $.post('functions', data, function() {
+      showList();
+    }, 'json');
+  } else {
+    $('#error').text('`Path` and `Code` are required!');
+  };
+}
+
+$(function() {
+  showList();
+});
